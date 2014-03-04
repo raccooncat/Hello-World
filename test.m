c07eb96936542a32b450a83fd0e61866ff950929
raccooncat <wulinxiaobao@gmail.com>
2014-03-04 11:33:43 +0800

    Create test.m

---
+++ test.m
@@ -0,0 +1,12 @@
+这是一段代码
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
